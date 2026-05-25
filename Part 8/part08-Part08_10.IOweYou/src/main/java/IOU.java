0db32d528695c5f34e80e9c9d0bc6339ab709a3a
hoyennhi489 <hyn489@gmail.com>
2026-05-25 23:21:53 +0700

    Part 8 - Short recap and HashMap exercises

---
+++ Part 8/part08-Part08_10.IOweYou/src/main/java/IOU.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class IOU {
+
+    private HashMap<String, Double> debts;
+
+    public IOU() {
+        this.debts = new HashMap<>();
+    }
+
+    public void setSum(String toWhom, double amount) {
+        debts.put(toWhom, amount);
+    }
+
+    public double howMuchDoIOweTo(String toWhom) {
+
+        if (debts.containsKey(toWhom)) {
+            return debts.get(toWhom);
+        }
+
+        return 0;
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+These are my solutions for Part I of the [Java Programming MOOC](https://java-programming.mooc.fi/) from the University of Helsinki.
