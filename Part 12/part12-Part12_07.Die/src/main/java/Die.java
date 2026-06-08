7521d7c6d2c345939b7bdc43c0e7d2569b59589b
hoyennhi489 <hyn489@gmail.com>
2026-06-08 19:35:16 +0700

    Part 12 - Randomness

---
+++ Part 12/part12-Part12_07.Die/src/main/java/Die.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class Die {
+
+    private Random random;
+    private int numberOfFaces;
+
+    public Die(int numberOfFaces) {
+        this.random = new Random();
+        this.numberOfFaces = numberOfFaces;
+    }
+
+    public int throwDie() {
+        return this.random.nextInt(this.numberOfFaces) + 1;
+    }
+}
